abe714acaebb32149be4943bc7450e3eba2b9f34
senalbulumulle <senal.bulumulle@gmail.com>
2022-04-17 11:35:56 -0400

    Renamed the ISO name to Smith-Pad-OS in profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="Smith-Pad-OS"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
