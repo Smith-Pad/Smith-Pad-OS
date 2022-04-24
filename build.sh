ab76d5cda62e1047e043f3da815d933e188311be
senalbulumulle <senal.bulumulle@gmail.com>
2022-04-24 10:26:55 -0400

    Fixed issue when generating/building files and ISO

--- build.sh
+++ build.sh
@@ -11,19 +11,9 @@
 ## Generates the ISO Image
 
 function generateISO() {
-	mkarchiso -v -w /tmp/Smith-Pad-OS-tmp/ archiso/
-}
-
-
-## This removes the temporary files that were created
-## during the ISO image creation process.
-
-function deleteTempFiles() {
-	sudo rm -rf /.-w/
-	sudo rm -rf work
+	sudo mkarchiso -v -w archiso archiso/ archiso/
 }
 
 
 
 generateISO
-deleteTempFiles
