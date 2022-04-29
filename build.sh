4aa2913b483e573f2c31c3a1662291c2f723f3f7
senalbulumulle <senal.bulumulle@gmail.com>
2022-04-29 11:54:58 -0400

    Created a function to cleanup the generated files that is used to create the ISO image

--- build.sh
+++ build.sh
@@ -14,6 +14,29 @@ function generateISO() {
 	sudo mkarchiso -v -w archiso archiso/ archiso/
 }
 
+function cleanup() {
+	cd archiso
+	sudo rm -rf base._cleanup_pacstrap_dir                          
+	sudo rm -rf base._make_bootmode_bios.syslinux.eltorito          
+	sudo rm -rf build._build_buildmode_iso
+	sudo rm -rf base._make_bootmode_bios.syslinux.mbr               
+	sudo rm -rf build_date
+	sudo rm -rf base._make_bootmode_uefi-x64.systemd-boot.eltorito  
+	sudo rm -rf base._make_bootmode_uefi-x64.systemd-boot.esp       
+	sudo rm -rf efiboot.img
+	sudo rm -rf base._make_boot_on_iso9660                          
+	sudo rm -rf iso/
+	sudo rm -rf base._make_custom_airootfs                          
+	sudo rm -rf iso._build_iso_image
+	sudo rm -rf base._make_customize_airootfs                       
+	sudo rm -rf iso.pacman.conf
+	sudo rm -rf base._make_packages                                 
+	sudo rm -rf base._make_pacman_conf                              
+	sudo rm -rf base._make_pkglist                                  
+	sudo rm -rf base._make_version                                  
+	sudo rm -rf base._mkairootfs_squashfs
+}
 
 
 generateISO
+cleanup
