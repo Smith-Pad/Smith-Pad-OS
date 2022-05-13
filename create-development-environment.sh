70f9a86424220fb182e6a2314c616419447d9d52
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-13 11:03:05 -0400

    mmmmm......

--- create-development-environment.sh
+++ create-development-environment.sh
@@ -38,8 +38,12 @@ function createFiles() {
 	echo """
 	cd DEVELOPMENT
 	cd Smith-Pad-OS
-	sudo rm -rf ../../TESTING
-	mkdir ../../TESTING
+	cd
+	cd Smith-Pad-OS-Development-Environment
+	sudo rm -rf TESTING
+	mkdir TESTING
+	cd DEVELOPMENT
+	cd Smith-Pad-OS
 	cp README.md ../../TESTING
 	cp build.sh ../../TESTING
 	cp -R archiso ../../TESTING
